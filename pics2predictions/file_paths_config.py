6fd570e594349b87c94f8be6a0c278505b0ad9ed
phillnguyen <46602608+phillnguyen@users.noreply.github.com>
2019-12-12 13:25:35 -0600

    Updated file_paths_config.py with new comments and path to big numpy data.

--- pics2predictions/file_paths_config.py
+++ pics2predictions/file_paths_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# data needs to be in this structure for daxgen_hyp workflow to work:
+# data needs to be in this structure workflow to work with hyperspectral images:
 # - data (does not have to be called data)
 #   - [plant folder name]
 #    - Hyp_SV_90
@@ -8,7 +8,7 @@
 #    - Hyp_SV_90
 #   ....
 
-# data needs to be in this structure for daxgen_npy workflow to work:
+# data needs to be in this structure for workflow to work with numpy arrays:
 # - data (does not have to be called data)
 #   - .npy
 #   - .npy
@@ -16,10 +16,10 @@
 
 
 # Note: There must be just plant folders in the data folder and each plant folder must contain a Hyp_SV_90 folder.
-# Example of data value for daxgen_hyp (Plant Folders in test_data): 'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_hyp/*/Hyp_SV_90/'
-# Example of data value for daxgen_npy (.npy in test_data): 'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_npy/*'
+# Example of data value for hyperspectral images (Plant Folders in test_data_hyp): 'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_hyp/*/Hyp_SV_90/'
+# Example of data value for numpy arrays (.npy in test_data_npy): 'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_npy/*'
 
-file_paths = {'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_hyp/*/Hyp_SV_90/',
+file_paths = {'data': '/work/csesd/pnnguyen/plant-phenotyping/pics2predictions/test_data_npy_big/*',
               'model': 'fold3_model_4_300_0.0005.h5'
 }
 
