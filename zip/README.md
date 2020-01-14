3107cb118fa621794b9817c06a4df0811fc81ca2
Phillip Nguyen <nguyennhphil1920@gmail.com>
2020-01-14 11:50:42 -0600

    Added new zip workflow.

---
+++ zip/README.md
@@ -0,0 +1,12 @@
+# pics2predictions Workflow
+
+Generating a Workflow
+---------------------
+Run the generate_dax.sh script.
+
+Running a Workflow
+-------------------
+Run the plan_dax.sh script.
+
+Here is the current workflow diagram (file names are used as examples):
+![](https://github.com/cseseniordesign/plant-phenotyping/blob/master/illustrations/Workflow%20Diagram.png)
---
+++ zip/daxgen.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import pwd
+import sys
+import time
+import file_paths_config as paths
+from glob import glob
+from Pegasus.DAX3 import *
+
+# The name of the DAX file is the first argument
+if len(sys.argv) != 2:
+        sys.stderr.write("Usage: %s DAXFILE\n" % (sys.argv[0]))
+        sys.exit(1)
+daxfile = sys.argv[1]
+
+USER = pwd.getpwuid(os.getuid())[0]
+
+# Create a abstract dag
+dax = ADAG("split")
+
+# Add some workflow-level metadata
+dax.metadata("creator", "%s@%s" % (USER, os.uname()[1]))
+dax.metadata("created", time.ctime())
+
+path_list = glob(paths.file_paths['data'])
+path_list_index = paths.file_paths['data'].split('/').index('*')
+
+for path in sorted(path_list):
+	plant_folder_name = path.split('/')[path_list_index]
+	preprocess = Job("zip")
+	zip_file_name = plant_folder_name + ".zip"
+	preprocess.addArguments("-r", zip_file_name, path_list)
+	dax.addJob(preprocess)
+	zip_file = File(zip_file_name)
+	preprocess.uses(zip_file, link=Link.OUTPUT, transfer=True, register=True)
+	preprocess.setStdout(zip_file)
+		
+
+
+f = open(daxfile, "w")
+dax.writeXML(f)
+f.close()
