e65ee927135f64f5ccf526c41a83bc3106299879
Phillip Nguyen <nguyennhphil1920@gmail.com>
2020-01-15 16:38:31 -0600

    Zip workflow now zips dataset.

--- zip/daxgen.py
+++ zip/daxgen.py
@@ -17,26 +17,28 @@ daxfile = sys.argv[1]
 USER = pwd.getpwuid(os.getuid())[0]
 
 # Create a abstract dag
-dax = ADAG("split")
+dax = ADAG("process")
 
 # Add some workflow-level metadata
 dax.metadata("creator", "%s@%s" % (USER, os.uname()[1]))
 dax.metadata("created", time.ctime())
 
+path_to_data = paths.file_paths['data'].replace('*','')
 path_list = glob(paths.file_paths['data'])
 path_list_index = paths.file_paths['data'].split('/').index('*')
 
 for path in sorted(path_list):
+	#cd = Job("cd")
+	#cd.addArguments(path_to_data)
+	#dax.addJob(cd)
 	plant_folder_name = path.split('/')[path_list_index]
 	preprocess = Job("zip")
 	zip_file_name = plant_folder_name + ".zip"
-	preprocess.addArguments("-r", zip_file_name, path)
+	preprocess.addArguments("-r", plant_folder_name, plant_folder_name)
 	zip_file = File(zip_file_name)
-	preprocess.uses(zip_file, link=Link.OUTPUT, transfer=True, register=True)
-	preprocess.setStdout(zip_file)
+	preprocess.uses(zip_file, link=Link.OUTPUT, transfer=True, register=False)
 	dax.addJob(preprocess)
-		
-
+	#dax.depends(child=preprocess, parent=cd)
 
 f = open(daxfile, "w")
 dax.writeXML(f)
